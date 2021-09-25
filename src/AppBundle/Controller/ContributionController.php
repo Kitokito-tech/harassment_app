29d25c9bab0f85d028023d37e43738a4a0b98621
Junpei Nitami <xintainjianjunpei@gmail.com>
2021-09-25 11:47:37 +0900

    ページネーション修正

--- src/AppBundle/Controller/ContributionController.php
+++ src/AppBundle/Controller/ContributionController.php
@@ -34,7 +34,7 @@ class ContributionController extends Controller
         $dateOrder = $request->query->get('form')['dateOrder'] ?? 'DESC';
         $search = $request->query->get('form')['search'] ?? '';
         $queries = ['cateIds' => $cateIds, 'dateOrder' => $dateOrder, 'search' => $search];
-        $results = $this->pagenation(1, $pageNum, $cateIds = $cateIds, $search = $search, $dateOrder = $dateOrder);
+        $results = $this->pagenation(10, $pageNum, $cateIds = $cateIds, $search = $search, $dateOrder = $dateOrder);
         if (!$results['pagesCount']) {
             $results['pagesCount'] = 1;
         }
